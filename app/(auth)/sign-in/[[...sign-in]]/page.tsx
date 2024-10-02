e09d373f0deda94050712a1ada0587f7faecf476
AdibHoque <adibhoque04@gmail.com>
2024-10-02 21:55:11 +0600

    Added Hero section

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import {SignIn} from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full my-6 flex justify-center items-center">
       <SignIn />;
     </div>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import {SignUp} from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full my-6 flex justify-center items-center">
       <SignUp />
     </div>
   );
