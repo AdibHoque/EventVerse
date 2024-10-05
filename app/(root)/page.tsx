8a510ecde2de73cc10874a1a40bdcf752ac37a45
AdibHoque <adibhoque04@gmail.com>
2024-10-05 19:57:18 +0600

    Setting up Mongoose and User Model

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <section className="bg-dotted-pattern bg-primary-50 bg-contain py-5 md:py-10">
+      <section
+        id="hero"
+        className="bg-dotted-pattern bg-primary-50 bg-contain py-5 md:py-10"
+      >
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
@@ -24,8 +27,19 @@ export default function Home() {
             alt="hero"
             width={1000}
             height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+            className="max-h-[80vh] object-contain object-center 2xl:max-h-[50vh]"
           />
+        </div>
+      </section>
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trusted by<br></br>Thousands of Events
+        </h2>
+        <div className="flex flex-col w-full gap-5 md:flex-row">
+          Search Category
         </div>
       </section>
     </>
