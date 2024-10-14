f2077a7ebccbbdf76aec31287f899972ede92038
AdibHoque <adibhoque04@gmail.com>
2024-10-14 19:03:28 +0600

    Incorporated UploadThing for uploading images

---
+++ components/ui/shared/Dropdown.tsx
@@ -0,0 +1,78 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import {ICategory} from "@/lib/database/models/category.model";
+import {startTransition, useState} from "react";
+import {Input} from "../input";
+import {SquarePlus} from "lucide-react";
+
+type DropdownProps = {
+  onChangeHandler?: () => void;
+  value?: string;
+};
+
+export default function Dropdown({onChangeHandler, value}: DropdownProps) {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [newCategory, setNewcategory] = useState("");
+
+  const handleAddCategory = () => {};
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem key={category._id} value={category._id}>
+              {category.name}
+            </SelectItem>
+          ))}
+
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full gap-1 items-center rounded-sm py-3 text-primary-500 focus:text-primary-500 hover:bg-primary-50 ">
+            <SquarePlus /> Add New Category
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewcategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => startTransition(handleAddCategory)}
+              >
+                Add
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+}
--- components/ui/shared/EventForm.tsx
+++ components/ui/shared/EventForm.tsx
@@ -14,9 +14,14 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {Textarea} from "@/components/ui/textarea";
+
 import {Input} from "@/components/ui/input";
 import {eventFormSchema} from "@/lib/validator";
 import {eventDefaultValues} from "@/constants";
+import Dropdown from "./Dropdown";
+import FileUploader from "./FileUploader";
+import {useState} from "react";
 
 type EventFormProps = {
   userId: string;
@@ -25,6 +30,7 @@ type EventFormProps = {
 
 export default function EventForm({userId, type}: EventFormProps) {
   const initialValues = eventDefaultValues;
+  const [files, setFiles] = useState<File[]>([]);
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
@@ -40,25 +46,81 @@ export default function EventForm({userId, type}: EventFormProps) {
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex flex-col gap-5"
       >
-        <div className="flex flex-col gap-5 md:flex-row"></div>
-        <FormField
-          control={form.control}
-          name="title"
-          render={({field}) => (
-            <FormItem className="w-full">
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="Event Title"
-                  {...field}
-                  className="input-field"
-                />
-              </FormControl>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({field}) => (
+              <FormItem className="w-full">
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Event Title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({field}) => (
+              <FormItem className="w-full">
+                <FormLabel>Category</FormLabel>
+                <FormControl>
+                  <Dropdown
+                    onChangeHandler={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({field}) => (
+              <FormItem className="w-full">
+                <FormLabel>Description</FormLabel>
+                <FormControl className="h-72">
+                  <Textarea
+                    placeholder="Event Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({field}) => (
+              <FormItem className="w-full">
+                <FormLabel>Image</FormLabel>
+                <FormControl className="h-72">
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  ></FileUploader>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
